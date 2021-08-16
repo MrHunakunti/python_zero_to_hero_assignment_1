595469b4235b4cc631693f427446d8db7461a7d7
MrHunakunti <69228219+MrHunakunti@users.noreply.github.com>
2021-08-16 09:42:50 -0700

    python_zero_to_hero_day_1 assignment submitted

---
+++ python_zero_to_hero_assignment_1.py
@@ -0,0 +1,10 @@
+"""PYTHON FROM ZERO TO HERO"""
+
+"""TAKING TWO USER INPUT STORING IT INTO TWO VARIABLES X AND Y THEN PERFORMING X^Y"""
+
+x = int(input("enter your first number: "))
+y = int(input("enter your first number: "))
+
+exponential_result = x**y
+
+print(f"result for {x}^{y} = {exponential_result}")
